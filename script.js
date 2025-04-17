fff9c663e96962d97511a9dde71a461b58916e0b
Gift Braimah <110871399+Gifted87@users.noreply.github.com>
2025-04-17 16:59:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -340,7 +340,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         // Assuming the command format is SET<index>:<number>, e.g., SET0:123456789
-        const command = `SET${index}:${numberToSet}`;
+        const command = `SET ${index},${numberToSet}`;
         sendSms(command);
         // Also save all numbers locally when one is updated/sent
         savePhoneNumbers();
